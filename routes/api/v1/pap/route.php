258a8c807c1961d175672995259ce45f0ee6c1cd
root <yan8msk@gmail.com>
2024-02-29 11:00:06 +0300

    update routs pap/sale/tron pap/sale/merchant001

--- routes/api/v1/pap/route.php
+++ routes/api/v1/pap/route.php
@@ -12,7 +12,7 @@ Route::namespace('Public')
             ->middleware('auth')
             ->group(function () {
                 Route::post('/signup', 'PapController@signup');
-                Route::post('/saletron', 'PapController@saleTron');
-                Route::post('/salemerchant001', 'PapController@saleMerchant001');
+                Route::post('/sale/tron', 'PapController@saleTron');
+                Route::post('/sale/merchant001', 'PapController@saleMerchant001');
             });
     });

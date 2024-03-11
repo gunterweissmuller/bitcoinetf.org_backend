882085ccdb042a6c3a6ff804ac64bc0d596aab48
vache <vachagan.aslanyan.1992@gmail.com>
2024-03-11 21:36:45 +0400

    feat: calculate trc amount on init payment case

--- app/Pipelines/V1/Public/Billing/Shares/Buy/Pipes/Init/ReplenishmentPipe.php
+++ app/Pipelines/V1/Public/Billing/Shares/Buy/Pipes/Init/ReplenishmentPipe.php
@@ -8,12 +8,14 @@ use App\Dto\DtoInterface;
 use App\Dto\Pipelines\Api\V1\Public\Billing\Shares\Buy\InitPipelineDto;
 use App\Pipelines\PipeInterface;
 use App\Services\Api\V1\Billing\ReplenishmentService;
+use App\Services\Api\V1\Settings\GlobalService;
 use Closure;
 
 final readonly class ReplenishmentPipe implements PipeInterface
 {
     public function __construct(
         private ReplenishmentService $replenishmentService,
+        private GlobalService $globalService,
     ) {
     }
 
@@ -23,8 +25,14 @@ final readonly class ReplenishmentPipe implements PipeInterface
 
         $btcPrice = $replenishment->getBtcPrice();
 
+        $trcBonus = number_format($this->globalService->getTrcBonus(), 8, '.', '');
+        $realAmount = $replenishment->getRealAmount();
+        if ($trcBonus > 0) {
+            $realAmount = $replenishment->getRealAmount() - $replenishment->getRealAmount() * $trcBonus;
+        }
+
         $replenishment->setAccountUuid($dto->getAccount()->getUuid());
-        $replenishment->setTotalAmount($replenishment->getReferralAmount() + $replenishment->getBonusAmount() + $replenishment->getRealAmount() + $replenishment->getDividendAmount());
+        $replenishment->setTotalAmount($replenishment->getReferralAmount() + $replenishment->getBonusAmount() + $realAmount + $replenishment->getDividendAmount());
         $replenishment->setTotalAmountBtc(1 / $btcPrice * $replenishment->getTotalAmount());
 
         $replenishment = $this->replenishmentService->create($replenishment);

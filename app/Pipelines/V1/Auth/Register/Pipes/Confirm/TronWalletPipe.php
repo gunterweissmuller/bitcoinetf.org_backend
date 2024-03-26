1ce1727b61e762a43da84929d1a6a22ae1942b94
yan <yan8msk@gmail.com>
2024-03-26 14:38:37 +0300

    TronWalletPipe copy to Confirm folder

---
+++ app/Pipelines/V1/Auth/Register/Pipes/Confirm/TronWalletPipe.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\Pipelines\V1\Auth\Register\Pipes\Confirm;
+
+use App\Dto\DtoInterface;
+use App\Dto\Pipelines\Api\V1\Public\Billing\Shares\Buy\InitPipelineDto;
+use App\Pipelines\PipeInterface;
+use App\Services\Api\V1\Users\AccountService;
+use Closure;
+use Exception;
+use Illuminate\Support\Facades\Http;
+
+final readonly class TronWalletPipe implements PipeInterface
+{
+    public function __construct(
+        private AccountService $accountService,
+    ) {
+    }
+
+    public function handle(InitPipelineDto|DtoInterface $dto, Closure $next): DtoInterface
+    {
+        try {
+            $account = $dto->getAccount();
+
+            if (!$account->getTronWallet()) {
+                $response = Http::baseUrl(env('PAYMENT_HOST'))
+                    ->withHeaders([
+                        'Authorization' => 'Bearer '.env('PAYMENT_API_KEY'),
+                    ])
+                    ->post('/v2/users/create', [
+                        'account_uuid' => $dto->getAccount()->getUuid(),
+                    ])
+                    ->throw()
+                    ->json();
+
+                $account->setTronWallet($response['wallet_uuid']);
+                $this->accountService->update([
+                    'uuid' => $account->getUuid(),
+                ], [
+                    'tron_wallet' => $response['wallet_uuid'] ?? null,
+                ]);
+                $dto->setAccount($account);
+            }
+        } catch (Exception $e) {
+            //throw new ReplenishmentNotAvailableException();
+        }
+
+        return $next($dto);
+    }
+}

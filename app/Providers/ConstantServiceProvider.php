4c5c2025974ecbfa5ff89b20cd249c4ff0e96597
yan <yan8msk@gmail.com>
2024-03-27 17:25:14 +0300

    income test wallet

--- app/Providers/ConstantServiceProvider.php
+++ app/Providers/ConstantServiceProvider.php
@@ -23,5 +23,6 @@ class ConstantServiceProvider extends ServiceProvider
         define('PAP_ACCOUNT_ID', 'bitcoinetf.postaffiliatepro.com');
         define('METAMASK_MSG', 'I fully understand and agree to the Terms and Conditions (available at https://bitcoinetf.org/terms) and certify that I am not a US citizen, resident or taxpayer.');
         define('MOON_PAY_CURRENCY_CODE', 'utk');
+        define('MOON_PAY_TEST_INCOME_WALLRT', '0xe175e4f1c662b2BD03F361a430453723f86c8f41');
     }
 }

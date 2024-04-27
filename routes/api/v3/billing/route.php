204c9529dbb33ce5d43e7e610c4f7ad4e27670a3
yaroslav <yan8msk@gmail.com>
2024-04-27 13:14:48 +0300

    fix check env 2

--- routes/api/v3/billing/route.php
+++ routes/api/v3/billing/route.php
@@ -39,8 +39,7 @@ Route::namespace('Public')
                                     ->prefix('apollopayment')
                                     ->group(function () {
                                         Route::post('/webhook/{account_uuid}', 'ApollopaymentController@webhook')->middleware([ApolloPaymentIp::class, ApolloPaymentSignature::class]);
-                                        //Route::post('/mockWebhook/{account_uuid}', 'ApollopaymentController@mockWebhook'); // @fixme-v delete after test
-                                        Route::post('/mockWebhook/{account_uuid}', 'ApollopaymentController@mockWebhook')->middleware([CheckEnvironment::class]); // @fixme-v delete after test
+                                        Route::post('/mockWebhook/{account_uuid}', 'ApollopaymentController@mockWebhook')->middleware([CheckEnvironment::class]);
                                     });
                                 // @fixme-v moonpay closed
                                 Route::namespace('MoonPay')

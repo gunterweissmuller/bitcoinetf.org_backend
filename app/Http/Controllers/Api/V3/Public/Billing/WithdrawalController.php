4b469b6376ab2aaf3a58fe3aef4d6c69476d5830
Yaroslav <yan8msk@gmail.com>
2024-06-05 18:27:35 +0300

    add mock webhook payout

--- app/Http/Controllers/Api/V3/Public/Billing/WithdrawalController.php
+++ app/Http/Controllers/Api/V3/Public/Billing/WithdrawalController.php
@@ -193,4 +193,32 @@ final class WithdrawalController extends Controller
 
         return response()->__call('exception', [$e]);
     }
+
+    /**
+     * @param WebhookRequest $request
+     * @return JsonResponse
+     */
+    public function mockWebhookPayout(WebhookRequest $request): JsonResponse
+    {
+        /*        {
+          "id": "2fa68ddf-2479-47cb-9e66-ae91139c3063",
+          "addressId": "dcb1a9fe-4b8d-40f6-baf6-241dc88436d9",
+          "userId": "6196a1f2-b6b5-40a5-a672-f1ffd70fdd7d",
+          "amount": "0.005",
+          "currency": "USDT",
+          "network": "bsc",
+          "addressFrom": ["0x....", "0x...."],
+          "addressTo": "0x....",
+          "status": "processed",
+          "confirmations": 10,
+          "tx": "0x5b9b3b55b366266025e",
+          "risks": {"level": "yellow", "categories": [{ "level": "yellow", "usdAmount": 41159.8, "category": "stolen funds", "service": "Reported as stolen funds bc1qlf4vel", "exposure": "DIRECT" }],
+          "createdAt": "2023-03-02T06:58:00.365Z",
+          "updatedAt": "2023-03-02T07:01:50.693Z",
+          "webhookId": "b614475d-aa39-49be-b3bf-1622e357a267"
+        }*/
+
+        return $this->webhookPayout($request);
+    }
+
 }

9dc0ffa923aaae163123e60386dbbbd782becb31
vache <vachagan.aslanyan.1992@gmail.com>
2024-06-27 19:16:36 +0400

    feat: add validation for buy init

---
+++ app/Pipelines/V3/Public/Billing/Shares/Buy/Pipes/Init/ValidatePipe.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Pipelines\V3\Public\Billing\Shares\Buy\Pipes\Init;
+
+use App\Dto\DtoInterface;
+use App\Dto\Pipelines\Api\V3\Public\Billing\Shares\Buy\InitPipelineDto;
+use App\Exceptions\Pipelines\V1\Billing\ReplenishmentNotAvailableException;
+use App\Pipelines\PipeInterface;
+use App\Services\Api\V1\Users\AccountService;
+use Closure;
+
+final readonly class ValidatePipe implements PipeInterface
+{
+    public function __construct(
+        private AccountService $accountService,
+    )
+    {
+    }
+
+    public function handle(InitPipelineDto|DtoInterface $dto, Closure $next): DtoInterface
+    {
+        $account = $this->accountService->get(['uuid' => $dto->getAccount()->getUuid()]);
+        if ($account->getOrderType() != null && $account->getOrderType() != $dto->getReplenishment()->getOrderType()) {
+            throw new ReplenishmentNotAvailableException();
+        }
+
+        return $next($dto);
+    }
+}
